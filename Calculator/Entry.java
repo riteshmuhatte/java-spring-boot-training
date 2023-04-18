d84f6bb15ab6f551bbcaec0d7598d4ba37371502
riteshmuhatte <riteshmuhatte@yahoo.com>
2023-04-18 10:29:11 +0530

    gitignore

--- Calculator/Entry.java
+++ Calculator/Entry.java
@@ -1,8 +1,8 @@
 public class Entry {
 	public static void main(String[] args) {
 		Maths m = new Maths();
-		int a = 10;
-		int b = 20;
+		int a = 7;
+		int b = 8;
 		
 		m.add(a, b);
 		m.multiply(a, b);
